6ee2c3dc9ccef6369e65ab62ee99bc106925e33f
Blarc <jakob.malezic@gmail.com>
2019-10-24 00:48:25 +0200

    PI naloga2: dodal clustering

--- PI/homework2_language_similarity/code/naloga2.py
+++ PI/homework2_language_similarity/code/naloga2.py
@@ -34,12 +34,14 @@ def read_files(dir_path):
 
 
 class MedoidClustering:
-    def __init__(self, data):
+    def __init__(self, data, k):
+        self.k = k
+
         self.data = data
 
         self.point_distances = {frozenset((c1, c2)): self.point_distance(c1, c2) for c1, c2 in
                                 combinations(self.data.keys(), 2)}
-        self.clusters = None
+        self.clusters = [[r] for r in self.data.keys()]
 
     def point_distance(self, r1, r2):
         dict_r1 = self.data[r1]
@@ -61,24 +63,41 @@ class MedoidClustering:
 
         return math.sqrt(sum_r1)
 
-    def cluster_distance(self, c1, c2):
-        pass
-
-    def closest_clusters(self):
-        pass
-
-    def get_random_leaders(self, k):
+    def get_random_leaders(self):
+        # TODO can be dict if slow
         leaders = []
         keys_list = list(self.data.keys())
-        while len(leaders) != k:
+        while len(leaders) != self.k:
             leader = random.choice(keys_list)
             keys_list.remove(leader)
             leaders.append(leader)
 
         return leaders
 
-    def run(self, k):
-        pass
+    def get_clusters(self):
+        leaders = self.get_random_leaders()
+        clusters = {leader: [leader] for leader in leaders}
+        for c in self.data.keys():
+            if c not in leaders:
+                min_dist = float("inf")
+                min_leader = None
+                for leader in leaders:
+                    dist = self.point_distances[frozenset((leader, c))]
+                    if dist < min_dist:
+                        min_dist = dist
+                        min_leader = leader
+
+                clusters[min_leader].append(c)
+
+        return clusters.values()
+
+    def min_in_cluster(self, cluster):
+
+        return [self.point_distances[frozenset((p1, p2))] for p1, p2 in combinations(cluster, 2)]
+
+    def run(self):
+        print(list(self.get_clusters())[0])
+        print(self.min_in_cluster(list(self.get_clusters())[0]))
 
 if __name__ == "__main__":
     DIR = "test"
@@ -86,6 +105,6 @@ if __name__ == "__main__":
     if platform.system() == "Linux":
         DIR_PATH = "/home/jakob/Documents/semester1_19-20/PI/homework2_language_similarity/data/" + DIR
     readData = read_files(DIR_PATH)
-    mc = MedoidClustering(readData)
-    mc.run(5)
+    mc = MedoidClustering(readData, 3)
+    mc.run()
     print("done")
