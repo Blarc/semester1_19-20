279bacac1822e518f6445e4ec3cfff2bef1c4ae7
Blarc <jakob.malezic@gmail.com>
2019-10-24 22:27:54 +0200

    PI naloga2: clustering done

--- PI/homework2_language_similarity/code/naloga2.py
+++ PI/homework2_language_similarity/code/naloga2.py
@@ -2,13 +2,20 @@ import math
 import os
 import platform
 import random
+import sys
 from collections import defaultdict
 from itertools import combinations
 
 from unidecode import unidecode
 
 
-def read_files(dir_path):
+def read_files(index_path, dir_path):
+    index_dict = defaultdict(str)
+    with open(index_path, encoding="utf8") as index:
+        for line in index:
+            split_line = line.split()
+            index_dict[split_line[0]] = split_line[1]
+
     data_dict = defaultdict(lambda: defaultdict(int))
 
     for file_name in os.listdir(dir_path):
@@ -28,7 +35,8 @@ def read_files(dir_path):
 
         file_name = file_name.replace(".txt", "")
         for i in range(0, len(data) - 2):
-            data_dict[file_name][data[i:i + 3]] += 1
+            key = index_dict[file_name]
+            data_dict[key][data[i:i + 3]] += 1
 
     return data_dict
 
@@ -52,7 +60,7 @@ class MedoidClustering:
             if key in dict_r2:
                 product_r1_r2 += (dict_r1[key] * dict_r2[key])
 
-        return product_r1_r2 / (self.point_len(r1) * self.point_len(r2))
+        return 1 - (product_r1_r2 / (self.point_len(r1) * self.point_len(r2)))
 
     def point_len(self, r1):
         dict_r1 = self.data[r1]
@@ -64,22 +72,19 @@ class MedoidClustering:
         return math.sqrt(sum_r1)
 
     def get_random_leaders(self):
-        # TODO can be dict if slow
-        leaders = []
+        leaders = defaultdict(list)
         keys_list = list(self.data.keys())
         while len(leaders) != self.k:
             leader = random.choice(keys_list)
             keys_list.remove(leader)
-            leaders.append(leader)
+            leaders[leader] = [leader]
 
         return leaders
 
-    def get_clusters(self):
-        leaders = self.get_random_leaders()
-        clusters = {leader: [leader] for leader in leaders}
+    def make_clusters(self, leaders):
         for c in self.data.keys():
             if c not in leaders:
-                min_dist = float("inf")
+                min_dist = sys.maxsize
                 min_leader = None
                 for leader in leaders:
                     dist = self.point_distances[frozenset((leader, c))]
@@ -87,24 +92,48 @@ class MedoidClustering:
                         min_dist = dist
                         min_leader = leader
 
-                clusters[min_leader].append(c)
+                leaders[min_leader].append(c)
 
-        return clusters.values()
+    def get_best_leader(self, cluster):
+        _, leader = min([(self.sum_all_distances(a, cluster), a) for a in cluster])
+        return leader
 
-    def min_in_cluster(self, cluster):
-
-        return [self.point_distances[frozenset((p1, p2))] for p1, p2 in combinations(cluster, 2)]
+    def sum_all_distances(self, a, cluster):
+        sum_a = 0
+        for b in cluster:
+            if a != b:
+                sum_a += self.point_distances[frozenset((a, b))]
+        return sum_a
 
     def run(self):
-        print(list(self.get_clusters())[0])
-        print(self.min_in_cluster(list(self.get_clusters())[0]))
+        leaders = self.get_random_leaders()
+
+        run = True
+        while run:
+            run = False
+            self.make_clusters(leaders)
+
+            new_leaders = defaultdict(list)
+            for leader, cluster in leaders.items():
+                best_leader = self.get_best_leader(cluster)
+                if best_leader != leader:
+                    run = True
+                new_leaders[best_leader] = [best_leader]
+
+            leaders = {leader: [leader] for leader in new_leaders}
+        self.make_clusters(leaders)
+
+        return leaders
+
 
 if __name__ == "__main__":
     DIR = "test"
-    DIR_PATH = "D:\Jakob\\3letnik\semester1\git\PI\homework2_language_similarity\data\\test"
+    DIR_PATH = "D:\Jakob\\3letnik\semester1\git\PI\homework2_language_similarity\data\\" + DIR
+    INDEX_PATH = "D:\Jakob\\3letnik\semester1\git\PI\homework2_language_similarity\data\INDEX.txt"
     if platform.system() == "Linux":
         DIR_PATH = "/home/jakob/Documents/semester1_19-20/PI/homework2_language_similarity/data/" + DIR
-    readData = read_files(DIR_PATH)
-    mc = MedoidClustering(readData, 3)
-    mc.run()
+        INDEX_PATH = None
+    readData = read_files(INDEX_PATH, DIR_PATH)
+    mc = MedoidClustering(readData, 5)
+    result = mc.run()
     print("done")
