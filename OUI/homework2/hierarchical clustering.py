70c45f11ab8364af5e8f7795aaee26dd121eacd6
Blarc <jakob.malezic@gmail.com>
2019-11-28 19:13:34 +0100

    OUI naloga2: 80% done, not sure if correct

---
+++ OUI/homework2/hierarchical clustering.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+import scipy.cluster.hierarchy as sch
+from matplotlib import pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
+
+NALOGA_8 = "clu/12.csv"
+NALOGA_9 = "clu/169.csv"
+
+DATA = pd.read_csv("./data/" + NALOGA_9)
+DATA.info()
+
+NUMBER_OF_ATTRIBUTES = len(DATA.columns)
+
+# naloga 8
+
+# cluster = AgglomerativeClustering(n_clusters=2, affinity='manhattan', linkage='single')
+# cluster.fit_predict(DATA)
+# print(cluster.labels_)
+#
+# ones = np.count_nonzero(cluster.labels_)
+# zeros = len(cluster.labels_) - ones
+#
+# print("ones:  %d" % ones)
+# print("zeros: %d" % zeros)
+
+# naloga 9
+
+# C1 = [4, 57]
+# C2 = [59, 79]
+#
+# K = 100
+#
+# kmeans = KMeans(n_clusters=2, random_state=0, max_iter=K, init=np.array([C1, C2]), n_init=1)
+# kmeans.fit(DATA)
+#
+# print(kmeans.labels_)
+# print(kmeans.cluster_centers_)
+#
+# ones = np.count_nonzero(kmeans.labels_)
+# zeros = len(kmeans.labels_) - ones
+# #
+# print("ones:  %d" % ones)
+# print("zeros: %d" % zeros)
+#
+#
+# print("End of program")
+
+# naloga 10
+
+# C1 = [87, -16]
+# C2 = [-48, -80]
+#
+# K = 100
+#
+# kmeans = KMeans(n_clusters=2, random_state=0, max_iter=K, init=np.array([C1, C2]), n_init=1)
+# kmeans.fit(DATA)
+#
+# print(kmeans.cluster_centers_)
+#
+# print("End of program")
--- OUI/homework2/linear_regression.py
+++ OUI/homework2/linear_regression.py
@@ -1,23 +1,106 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, KFold
 
-data = pd.read_csv("./data/reg/167.csv")
+from sklearn.linear_model import *
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
 
-data.head()
 
+X0 = "X0"
 X9 = "X9"
 Y = "Y"
 
-# bins = np.linspace(0, max(data[X9]), 50)
-#
-# mhv = np.digitize(data[X9], bins)
-#
-# L, T = train_test_split(data, test_size=0.2, random_state=42, stratify=mhv)
+NALOGA_1 = "reg/167.csv"
+NALOGA_2 = "reg/8.csv"
+NALOGA_3 = "reg/26.csv"
+NALOGA_4 = "reg/41.csv"
+NALOGA_5 = "reg/154.csv"
+NALOGA_6 = "reg/166.csv"
 
-C = data.corr()
-print(C[Y].sort_values(ascending=False))
 
-print("Hello World!")
+DATA = pd.read_csv("./data/" + NALOGA_6)
+DATA.info()
+
+NUMBER_OF_ATTRIBUTES = len(DATA.columns) - 1
+
+# naloga 1
+# C = DATA.corr()
+# print(C[Y].sort_values(ascending=False))
+
+# naloga 2 / naloga 4
+# # Split to learning and test set
+# L, T = train_test_split(DATA, test_size=0.2)
+#
+# # naloga 5
+# # Split to learning and test set
+# # SIZE_LEARN = 37
+# # L = DATA[:SIZE_LEARN]
+# # T = DATA[SIZE_LEARN:]
+# #
+# print("Learning set size: {:d}\nTest set size: {:d}\n".format(len(L), len(T)))
+#
+# # Split learning set to attributes and classes
+# learning_x = L.iloc[:, 0:NUMBER_OF_ATTRIBUTES]
+# learning_y = L.iloc[:, NUMBER_OF_ATTRIBUTES:]
+#
+# # Split testing set to attributes and classes
+# testing_x = T.iloc[:, 0:NUMBER_OF_ATTRIBUTES]
+# testing_y = T.iloc[:, NUMBER_OF_ATTRIBUTES:]
+#
+# regr = LinearRegression()
+# # Train the model using training sets
+# regr.fit(learning_x, learning_y)
+#
+# # Make predictions for testing set
+# testing_pred = regr.predict(testing_x)
+#
+# # The coefficients
+# print('Coefficients: \n', regr.coef_)
+# # The mean squared error
+# print("Mean squared error: %.6f" % mean_squared_error(testing_y, testing_pred))
+# # Explained variance score: 1 is perfect prediction
+# print('Variance score: %.6f' % r2_score(testing_y, testing_pred))
+
+# naloga 6
+# # Split to learning and test set
+# K = 10
+# SIZE_LEARN = 23
+# # L = DATA[:SIZE_LEARN]
+# # T = DATA[SIZE_LEARN:]
+#
+# # learning_x = L.iloc[:, 0:NUMBER_OF_ATTRIBUTES]
+# # learning_y = L.iloc[:, NUMBER_OF_ATTRIBUTES:]
+#
+# model = KNeighborsRegressor(n_neighbors=3)
+# scores = []
+#
+# DATA.sample(frac=1)
+# L = DATA[:SIZE_LEARN]
+# T = DATA[SIZE_LEARN:]
+#
+# # Split learning set to attributes and classes
+# learning_x = L.iloc[:, 0:NUMBER_OF_ATTRIBUTES]
+# learning_y = L.iloc[:, NUMBER_OF_ATTRIBUTES:]
+#
+#
+#
+# for test_set in T:
+#     # Split testing set to attributes and classes
+#     testing_x = test_set.iloc[:, 0:NUMBER_OF_ATTRIBUTES]
+#     testing_y = test_set.iloc[:, NUMBER_OF_ATTRIBUTES:]
+#
+#
+#
+# # model.fit(learning_x, learning_y)
+# # score = model.score(testing_x, testing_y)
+
+# naloga 8
+
+
+
+
+print("End of program")
